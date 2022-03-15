8a09a06f0dcc64f9ca289d6df3555bc0a59f62b2
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-15 13:16:09 +0300

    Update made all required changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
-
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
