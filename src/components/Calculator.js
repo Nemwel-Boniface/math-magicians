968cc6427d3ed7f46614c9de716fbe9a8e4c5ba8
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-15 06:55:00 +0300

    Update include Calculator basic UI

---
+++ src/components/Calculator.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  state = {
+    val: 0,
+   }
+  render() {
+    return (
+      <React.Fragment>
+        <div className='resultsPanel'>
+          <span className='result'>{this.state.val}</span>
+        </div>
+        <div className='buttons'>
+        <button type="button" className="button-1 top-buttons" >AC</button>
+        <button type="button" className="button-2 top-buttons" >+/-</button>
+        <button type="button" className="button-3 top-buttons" >%</button>
+        <button type="button" className="button-4 x right-buttons" >÷</button>
+        <button type="button" className="button-5 central-buttons" >7</button>
+        <button type="button" className="button-6 central-buttons" >8</button>
+        <button type="button" className="button-7 central-buttons" >9</button>
+        <button type="button" className="button-8 x right-buttons" >x</button>
+        <button type="button" className="button-9 central-buttons" >4</button>
+        <button type="button" className="button-10 central-buttons" >5</button>
+        <button type="button" className="button-11 central-buttons" >6</button>
+        <button type="button" className="button-12 x right-buttons" >-</button>
+        <button type="button" className="button-13 central-buttons" >1</button>
+        <button type="button" className="button-14 central-buttons" >2</button>
+        <button type="button" className="button-15 central-buttons" >3</button>
+        <button type="button" className="button-16 x right-buttons" >+</button>
+        <button type="button" className="button-17 central-buttons" >0</button>
+        <button type="button" className="button-18 central-buttons" >.</button>
+        <button type="button" className="button-19 x right-buttons" >=</button>
+      </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Calculator;
