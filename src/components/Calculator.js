2caf9c3c2d6cf86f7241624bc4156398c46b5d7c
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-16 15:16:57 +0300

    Update included math logic to project

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,40 +1,51 @@
 import React, { Component } from 'react';
 import calculate from '../logic/calculate';
-import operate from '../logic/operate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { val: 5 };
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleCalc = (e) => {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
-    const { val } = this.state;
+    const { total, next, operation } = this.state;
     return (
       <>
         <div className="resultsPanel">
-          <span className="result">{ val }</span>
+          <span className="result">
+            {total}
+            {operation}
+            {next}
+          </span>
         </div>
         <div className="buttons">
-          <button type="button" className="button-1 top-buttons">AC</button>
-          <button type="button" className="button-2 top-buttons">+/-</button>
-          <button type="button" className="button-3 top-buttons">%</button>
-          <button type="button" className="button-4 x right-buttons">÷</button>
-          <button type="button" className="button-5 central-buttons">7</button>
-          <button type="button" className="button-6 central-buttons">8</button>
-          <button type="button" className="button-7 central-buttons">9</button>
-          <button type="button" className="button-8 x right-buttons">x</button>
-          <button type="button" className="button-9 central-buttons">4</button>
-          <button type="button" className="button-10 central-buttons">5</button>
-          <button type="button" className="button-11 central-buttons">6</button>
-          <button type="button" className="button-12 x right-buttons">-</button>
-          <button type="button" className="button-13 central-buttons">1</button>
-          <button type="button" className="button-14 central-buttons">2</button>
-          <button type="button" className="button-15 central-buttons">3</button>
-          <button type="button" className="button-16 x right-buttons">+</button>
-          <button type="button" className="button-17 central-buttons">0</button>
-          <button type="button" className="button-18 central-buttons">.</button>
-          <button type="button" className="button-19 x right-buttons">=</button>
+          <button onClick={this.handleCalc} type="button" className="btn1">AC</button>
+          <button onClick={this.handleCalc} type="button" className="btn2">+/-</button>
+          <button onClick={this.handleCalc} type="button" className="btn3">%</button>
+          <button onClick={this.handleCalc} type="button" className="btn4 x">÷</button>
+          <button onClick={this.handleCalc} type="button" className="btn5">7</button>
+          <button onClick={this.handleCalc} type="button" className="btn6">8</button>
+          <button onClick={this.handleCalc} type="button" className="btn7">9</button>
+          <button onClick={this.handleCalc} type="button" className="btn8 x">x</button>
+          <button onClick={this.handleCalc} type="button" className="btn9">4</button>
+          <button onClick={this.handleCalc} type="button" className="btn0">5</button>
+          <button onClick={this.handleCalc} type="button" className="btn11">6</button>
+          <button onClick={this.handleCalc} type="button" className="btn12 x">-</button>
+          <button onClick={this.handleCalc} type="button" className="btn13">1</button>
+          <button onClick={this.handleCalc} type="button" className="btn14">2</button>
+          <button onClick={this.handleCalc} type="button" className="btn15">3</button>
+          <button onClick={this.handleCalc} type="button" className="btn16 x">+</button>
+          <button onClick={this.handleCalc} type="button" className="btn17 double">0</button>
+          <button onClick={this.handleCalc} type="button" className="btn18">.</button>
+          <button onClick={this.handleCalc} type="button" className="btn19 x">=</button>
         </div>
       </>
     );
