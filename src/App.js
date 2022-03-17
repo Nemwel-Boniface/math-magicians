c7d528dceabe6ea33355c18ed5c2a7c64a793f58
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-17 12:22:32 +0300

    Update refactored APP.js stateless componente

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import './App.css';
 import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
-  }
+const App = () => {
+  const state = {};
 
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
+
 
 export default App;
