5c9c6297a9c341e17a2ce05695e72a79fd5f2fc2
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-17 12:25:55 +0300

    Update fixed all App.js linter errors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-const App = () => {
-  const state = {};
-
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
-}
-
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;
