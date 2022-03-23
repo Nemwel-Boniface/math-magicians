db711e501aff909c9b1920a00004abc595af64ce
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-23 17:09:51 +0300

    Update gave the individual links unique id

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div className="navBar">
+  <div data-testid="Navbar" className="navBar">
     <nav>
       <h1>Math Magicians</h1>
       <ul>
-        <li className="bar"><Link to="/">Home</Link></li>
-        <li className="bar"><Link to="/calculator">Calculator</Link></li>
-        <li><Link to="/quote">Quote</Link></li>
+        <li className="bar"><Link data-testid="home-link" to="/">Home</Link></li>
+        <li className="bar"><Link data-testid="calculator-link" to="/calculator">Calculator</Link></li>
+        <li><Link data-testid="quote-link" to="/quote">Quote</Link></li>
       </ul>
     </nav>
   </div>
